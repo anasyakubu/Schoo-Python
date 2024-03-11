5e00087c30bc818d1cd7a024e2429cb5b080a76a
anasyakubu <yakubuanas04@gmail.com>
2024-03-11 15:04:53 +0100

    Def Function

--- loop.py
+++ loop.py
@@ -5,11 +5,11 @@
 # for i in range(1, 20, 3):
 #     print(i, end=" ")
 
-# def function1(a,b) :
-#   result = 0
-#   for i in range(a,b):
-#     return result
-# print("the answer is",function1(10,30))
+def function1(a,b) :
+  result = 0
+  for i in range(a,b):
+    return result
+print("the answer is",function1(10,30))
 
 for i in range(1, 101):
     if i % 5 != 0 and i % 7 != 0:
